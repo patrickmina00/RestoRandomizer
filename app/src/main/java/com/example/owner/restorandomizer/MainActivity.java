169f5bc63a6d10ad35fbb94fdb225a110e4c2b6c
patrickmina00 <patrick.mina00@gmail.com>
2016-05-01 10:17:11 +1200

    Automatic Checkbox filler added

--- app/src/main/java/com/example/owner/restorandomizer/MainActivity.java
+++ app/src/main/java/com/example/owner/restorandomizer/MainActivity.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.text.Layout;
 import android.view.LayoutInflater;
@@ -33,6 +35,10 @@ public class MainActivity extends AppCompatActivity {
     private List<String> restos = new ArrayList<String>();
     private String currentResto;
     private final Context context = this;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
 
     /*****
      * Animation for Sliding Menu
@@ -47,6 +53,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //Adding RecyclerView
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mAdapter = new MyAdapter(this.restos);
+        mRecyclerView.setAdapter(mAdapter);
 
 
         //Adding Buttons in
@@ -262,6 +276,10 @@ public class MainActivity extends AppCompatActivity {
 
     public LinearLayout getLin() {
         return (LinearLayout) findViewById(R.id.slider);
+    }
+
+    public List<String> getRestos(){
+        return this.restos;
     }
 
     /**
